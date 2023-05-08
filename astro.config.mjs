cb585528faadefaab6dda67619384cf033e2f690
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2023-05-09 00:12:30 +0700

    feat: https for pnpm dev:https

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import node from "@astrojs/node";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://localhost:3000',
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  vite: {
+    plugins: [ basicSsl() ]
+  }
 });
