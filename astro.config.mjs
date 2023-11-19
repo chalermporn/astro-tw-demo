dbaa9fd008ab9fb88c53a9e3fab3558f192d50cf
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2023-11-19 11:21:02 +0700

    feat: tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import node from "@astrojs/node";
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://localhost:3000',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: node({
     mode: "standalone"
   }),
   vite: {
-    plugins: [ basicSsl() ]
+    plugins: [basicSsl()]
   }
 });
